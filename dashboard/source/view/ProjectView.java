27a492ec6c112411ad8cc0c109327ebefac5f9ad
princess <princess>
2006-01-23 17:12:15 +0000

    création de la classe permettant d'obtenir une vue globale sur le projet. Elle sera codée par 2 L3.

---
+++ dashboard/source/view/ProjectView.java
@@ -0,0 +1,19 @@
+package view;
+
+import control.ProjectControl;
+
+/* Cette classe permet d'obtenir une vision globale sur l'avancement du projet
+ *  en ayant actuellement 3 principaux indicateurs:	
+ *  	avancement par rapport aux estimations
+ *  	rapport entre le budget consommé et le temps effectif
+ *  	etat du budget en fonction des estimations
+ */
+
+public class ProjectView {
+
+	ProjectControl pc = new ProjectControl();
+	/*
+	 * pc permet de récupérer les indicateurs calculés 
+	 * dans la classe control: ProjectControl
+	 */
+}

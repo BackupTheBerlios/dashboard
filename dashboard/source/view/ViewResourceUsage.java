724958d9bf76dd1765e71ddaf795df6c5506415e
simonnet <simonnet>
2006-02-21 21:18:19 +0000

    *** empty log message ***

--- dashboard/source/view/ViewResourceUsage.java
+++ dashboard/source/view/ViewResourceUsage.java
@@ -3,41 +3,20 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.lang.reflect.Array;
+
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
-import java.util.Vector;
-
-import javax.swing.ButtonGroup;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JRadioButton;
+
 import javax.swing.WindowConstants;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.CategoryAxis;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.axis.NumberTickUnit;
-import org.jfree.chart.plot.CategoryPlot;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.renderer.category.BarRenderer;
-import org.jfree.data.category.DefaultCategoryDataset;
+
 import org.jfree.data.general.DefaultPieDataset;
-
-import control.ControlEnvironment;
-import control.ControlProject;
-import control.objVariation;
-import examples.TestEntity;
-
 
 public class ViewResourceUsage extends JFrame
 {
@@ -54,14 +33,13 @@ private Container container;
 			
 			// pie chart 
 			DefaultPieDataset pieData = this.extractPieDataSet(map) ;
-			JFreeChart chart = ChartFactory.createPieChart3D("Utilisation des ressources" ,pieData,true,false,false);
+			JFreeChart chart = ChartFactory.createPieChart("Utilisation des ressources" ,pieData,true,false,false);
 			p = new ChartPanel(chart) ;
 			this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 			this.setSize(400,400) ;
 			Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 			this.setLocation((screen.width - this.getSize().width)/2,(screen.height - this.getSize().height)/2); 
 			this.setVisible(true);
-
 			p.setVisible(true); 
 			container.add(p,BorderLayout.CENTER) ;
 			

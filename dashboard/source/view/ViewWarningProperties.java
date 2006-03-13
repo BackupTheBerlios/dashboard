c4e5a38ef42dadb0ceff5a1e9eed26f33483028a
l3isi25 <l3isi25>
2006-03-13 10:09:28 +0000

    *** empty log message ***

--- dashboard/source/view/ViewWarningProperties.java
+++ dashboard/source/view/ViewWarningProperties.java
@@ -1,31 +1,18 @@
 package view;
 
-import control.ControlPlannable;
-import entity.WorkBreakDownElement;
 import entity.WarningLimit;
-
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.HashMap;
+
 
 
 import javax.swing.*;
 
-/**
-* This code was edited or generated using CloudGarden's Jigloo
-* SWT/Swing GUI Builder, which is free for non-commercial
-* use. If Jigloo is being used commercially (ie, by a corporation,
-* company or business for any purpose whatever) then you
-* should purchase a license for each developer using Jigloo.
-* Please visit www.cloudgarden.com for details.
-* Use of Jigloo implies acceptance of these licensing terms.
-* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
-* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
-* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
-*/
+
 public class ViewWarningProperties extends JDialog{
 	private JLabel jLabel1;
 	private JLabel jLabel2;
@@ -33,37 +20,40 @@ public class ViewWarningProperties extends JDialog{
 	private JTextField field2;
 	private JButton ok;
 	private JButton annuler;
+	private JPanel centre;
+	private JPanel bas;
 	
 	public ViewWarningProperties()
 	{
-		WarningLimit.setMinLimit(2);
-		this.field1 =new JTextField(WarningLimit.getMinLimit());
-		this.field2 =new JTextField(WarningLimit.getMaxLimit());
+		
+		this.field1 =new JTextField();
+		this.field2 =new JTextField();
 		this.jLabel1=new JLabel("Seuil minimal (en %)");
 		this.jLabel1.setPreferredSize(new Dimension (150,150));
 		this.jLabel2=new JLabel("Seuil maximal (en %)");
 		this.ok=new JButton("OK");
+		this.ok.setDefaultCapable(true);
 		this.annuler=new JButton("annuler");
-		JPanel centre =new JPanel();
-		JPanel bas = new JPanel();
+		centre =new JPanel();
+		bas = new JPanel();
 		
 		this.setTitle("Réglages des seuils d'alerte");
 		this.setSize(300,125);
-		
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setLocation((screen.width - this.getSize().width)/2,(screen.height - this.getSize().height)/2); 	
 		this.setLayout(new BorderLayout());
 		
+		
+		this.field1.setText(""+WarningLimit.getMinLimit());
+		this.field2.setText(""+WarningLimit.getMaxLimit());
 		centre.setLayout(new GridLayout (2,2));
 		centre.add(this.jLabel1);
-		
-		// jen suis là
-		
-		this.field2.setSize(3,1);
 		centre.add(this.field1);
 		centre.add(this.jLabel2);
 		centre.add(this.field2);
-		
 		bas.add(this.ok);
 		bas.add(this.annuler);
+		
 		this.annuler.addActionListener(new ActionListener ()
 				{public void actionPerformed (ActionEvent ev)
 					{ViewWarningProperties.this.dispose();}
@@ -72,22 +62,45 @@ public class ViewWarningProperties extends JDialog{
 	                {
 		    			public void actionPerformed (ActionEvent ev)
 		    			{	boolean numeric=true;
-		    			
+		    				String message="";
+		    				/* test if the JTextField contents are numeric*/
 		    				for(int i=0;i<ViewWarningProperties.this.field1.getText().length()&&numeric;i++)
 		    				{
-		    					
 		    					numeric=Character.isDigit(ViewWarningProperties.this.field1.getText().charAt(i));
 		    				}
 		    				for(int i=0;i<ViewWarningProperties.this.field2.getText().length()&&numeric;i++)
 		    				{
-		    					
+		 
 		    					numeric=Character.isDigit(ViewWarningProperties.this.field2.getText().charAt(i));
 		    				}
-		    				if (!numeric ||ViewWarningProperties.this.field2.getText().equals("")||ViewWarningProperties.this.field1.getText().equals(""))
+		    				
+		    				/* test if the JTextField contents are empty*/
+		    				if(ViewWarningProperties.this.field2.getText().equals("")||ViewWarningProperties.this.field1.getText().equals(""))
 		    				{
-		    					JOptionPane.showMessageDialog(ViewWarningProperties.this, "Les seuils saisis sont incorrects",
-		    						      "Erreur",
+		    					numeric=false;
+		    				}
+		    				/*test (0<content<100) */
+		    				if (numeric&&(Integer.parseInt(ViewWarningProperties.this.field1.getText())<0 ||
+		    								Integer.parseInt(ViewWarningProperties.this.field1.getText())>100 ||
+		    								Integer.parseInt(ViewWarningProperties.this.field2.getText())<0||
+		    								Integer.parseInt(ViewWarningProperties.this.field2.getText())>100))
+		    				{
+		    					message+="- Les seuils doivent être compris entre 0 et 100%\n";
+		    				}
+		    				
+		    				/*test (min<max)*/
+    						if (numeric&&(Integer.parseInt(ViewWarningProperties.this.field1.getText())>Integer.parseInt(ViewWarningProperties.this.field2.getText())))
+    						{
+    							message+="- Le seuil minimum doit être inférieur au seuil maximum \n";
+    						}
+		    
+		    				if (!numeric ||!message.equals(""))
+		    				{
+		    					
+		    						JOptionPane.showMessageDialog(ViewWarningProperties.this, "Les seuils saisis sont incorrects\n"+message,
+		    								"Erreur",
 		    						      JOptionPane.WARNING_MESSAGE);
+		    					
 		    				}else{
 		    					WarningLimit.setMinLimit(Integer.parseInt(ViewWarningProperties.this.field1.getText()));
 		    					WarningLimit.setMaxLimit(Integer.parseInt(ViewWarningProperties.this.field2.getText()));
@@ -101,12 +114,6 @@ public class ViewWarningProperties extends JDialog{
 		this.add(centre,BorderLayout.CENTER);
 		this.add(bas,BorderLayout.SOUTH);
 		this.setVisible(true);
-	}
-	public static void main(String args[]) {
-		
-		ViewWarningProperties v = new ViewWarningProperties();
-		
-		
 	}
 	
 	}
